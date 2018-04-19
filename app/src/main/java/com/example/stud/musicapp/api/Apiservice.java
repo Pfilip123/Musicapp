8197ece5d11fef5c8320f6a4fdc5029751a246ff
W57003 <pfman@o2.pl>
2018-04-19 18:19:52 +0200

    zmiana api

--- app/src/main/java/com/example/stud/musicapp/api/Apiservice.java
+++ app/src/main/java/com/example/stud/musicapp/api/Apiservice.java
@@ -2,12 +2,15 @@ package com.example.stud.musicapp.api;
 
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Apiservice {
     public static ApiClient getService() {
         Retrofit retrofit = new Retrofit.Builder()
-               .baseUrl( "http://www.theaudiodb.com/api/v1/json/{APIKEY}/" )
+               .baseUrl("http://www.theaudiodb.com/api/v1/json/{APIKEY}/")
+                .addConverterFactory(GsonConverterFactory.create ())
                 .build();
         return retrofit.create(ApiClient. class );
+
     }
 }
