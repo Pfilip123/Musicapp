12075bf6dd57dbf74b6202456340d8de8d737760
W57003 <pfman@o2.pl>
2018-04-26 17:45:31 +0200

    lab6

---
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsAdapter.java
@@ -0,0 +1,12 @@
+package com.example.stud.musicapp.topsongs;
+
+/**
+ * Created by W57003 on 2018-04-26.
+ */
+
+public class TopSongsAdapter {
+}
+
+public int getItemCount(){
+    return this.trending != null ? this.trending.size() : 0
+}
