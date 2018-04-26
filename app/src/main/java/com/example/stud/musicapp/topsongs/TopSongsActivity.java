c855289430974d42560248e6f693fc027ded17aa
W57003 <pfman@o2.pl>
2018-04-26 18:11:19 +0200

    lista utworów (błąd)

--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -3,6 +3,7 @@ package com.example.stud.musicapp.topsongs;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
@@ -10,7 +11,11 @@ import android.widget.Toast;
 import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.api.Apiservice;
 import com.example.stud.musicapp.api.TrendingList;
+import com.example.stud.musicapp.api.TrendingSingle;
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,14 +24,24 @@ import retrofit2.Response;
 
 public class TopSongsActivity extends AppCompatActivity {
 
+
     RecyclerView rvList;
+    List<TrendingSingle> trendingSingles = new ArrayList<>(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_songs);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled( true ); //górny pasek aplikacji
+        TopSongsAdapter topSongsAdapter = new TopSongsAdapter(trendingSingles);
+        rvList.setAdapter(topSongsAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+rvList.setLayoutManager(linearLayoutManager);
 
         rvList = findViewById(R.id.rvList);
 
@@ -37,7 +52,12 @@ public class TopSongsActivity extends AppCompatActivity {
             public void onResponse(@NonNull Call<TrendingList> call, @NonNull
                     Response<TrendingList> response) {
                 TrendingList trendingList = response.body();
-                Log.d( "TAG" , new Gson().toJson(trendingList));
+
+                if (trendingList != null ) {
+                    TopSongsActivity. this.trendingSingles.clear();
+                    TopSongsActivity. this.trendingSingles.addAll(trendingList.trending );
+                    TopSongsActivity. this.rvList.getAdapter().notifyDataSetChanged();
+                }
             }
 
             @Override
