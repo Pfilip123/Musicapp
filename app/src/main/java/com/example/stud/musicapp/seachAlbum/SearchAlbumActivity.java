4861379e7507536c42c08c7a493fb025bd1cec35
W57003 <pfman@o2.pl>
2018-05-24 19:20:16 +0200

    wyłapany błąd

--- app/src/main/java/com/example/stud/musicapp/seachAlbum/SearchAlbumActivity.java
+++ app/src/main/java/com/example/stud/musicapp/seachAlbum/SearchAlbumActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Button;
@@ -12,62 +13,69 @@ import android.widget.EditText;
 import com.example.stud.musicapp.R;
 
 public class SearchAlbumActivity extends AppCompatActivity {
+    EditText etQuery;
+    RecyclerView rvList;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_album);
-        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
-        SharedPreferences sharedPreferences;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         sharedPreferences = getPreferences(MODE_PRIVATE);
-
-        EditText etQuery;
-        RecyclerView rvList;
-
-
-        String album = sharedPreferences .getString( "query" , null );
-        etQuery.setText(artist);
-
 
         etQuery = findViewById(R.id.etQuery);
         rvList = findViewById(R.id.rvList);
 
+        String artist = sharedPreferences.getString("query", null);
+        etQuery.setText(artist);
 
 
-        Button bSearch = findViewById(R.id.bSearch);
-    bSearch.setOnClickLisner(new View.OnClickListener() {
+        try {
+            int zapisanaWartosc = sharedPreferences.getInt("moja_wartosc", 0);
+        } catch (ClassCastException e) {
+            Log.e("TAG", "Inny wyjątek", e);
+        }
+    }
 
-        getSupportActionBar();
 
+    Button bSearch = findViewById(R.id.bSearch);
+    bSearch.setOnClickListner(new View.OnClickListener()
+
+    {
+
+        public void onClickView (View view){
+
+        String query = etQuery.getText().toString();
+        rememberQuery(query);
+
+    }
+    });
+}
+private void rememberQuery(String query) {
+
+}
+    getSupportActionBar();
         setDisplayHomeAsUpEnabled(true);
-
         sharedPreferences = getPreferences(MODE_PRIVATE);
 
         String query = etQuery.getText().toString();
 
         rememberQuery(query);
 
-        public void onClickView(View view) {
-            String query = etQuery.getText().toString();
-            rememberQuery(query);
-
-        }
-
     });
 
+    private void rememberQuery(String query) {
 
-    private void rememberQuery (String query) {
-
-        SharedPreferences.Editor editor = SharedPreferences.edit();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("query", query);
         editor.apply();
     }
 
 
-
     public boolean onSupportNavigateUp() {
         onBackPressed();
-        return true ;
+        return true;
     }
-    }
-
+}
+}
